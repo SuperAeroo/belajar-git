454c80a0715ec7c80e8f1bdd21b8c55ea3e666db
aldii <200401141@student.umri.ac.id>
2023-09-06 22:10:04 +0700

    add detail tutor

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ class Animal{
 /*
     1. git init
         -> menginisialisasi project git baru
-    2. git add
+    2. git add namafile.extension
         -> memindahkan file kedalam staging area
     3. git commit -m "commit message"
         -> menambahkan berita acara
         -> memindahkan perubahan kedalam local repository
     4. git status
         -> untuk melihat status perubahan
-    5. git remote add
+    5. git remote add origin link github
         -> menambahkan link remote repository
     6. git remote -v
         -> melihat remote yang ada
-    7. git push
+    7. git push -u origin master(master tergantung dari branch)
         -> upload dari local repository ke remote repository
     8. git pull
         -> download dari remote repository ke local repository
